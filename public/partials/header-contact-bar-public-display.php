92d1a617cd21dda790917f105dfaaf59bf312937
Gary Swift <garyswiftmail@gmail.com>
2017-07-19 12:24:54 +0100

    Added sass and css

--- public/partials/header-contact-bar-public-display.php
+++ public/partials/header-contact-bar-public-display.php
@@ -27,8 +27,8 @@ if (function_exists('get_field')) {
 }
 ?>
 <div class="header-links black-bar" id="header-links">
-	<div class="not-row">
-		<a href="#" id="show-map-button" class="close-map active effect"><i class="fa fa-map-marker"></i> Find Our Location</a><!-- 
+	<div class="not-row"><!-- 
+	     --><a href="#" id="show-map-button" class="close-map active effect"><i class="fa fa-map-marker"></i> Find Our Location</a><!-- 
 		 --><a href="tel:<?php echo $phone; ?>" class="effect"><i class="fa fa-volume-control-phone"></i> Call Today! <?php echo $phone_readable; ?></a><!-- 
 		 --><a href="mailto:<?php echo $email ?>" class="effect"><i class="fa fa-envelope-o"></i> <?php echo $email ?></a>
 		<?php //include 'template-parts/_social-media-links.php'; ?>
@@ -38,17 +38,17 @@ if (function_exists('get_field')) {
 				<?php $social_media_links = get_social_media(); ?>
 
 				<?php if ( is_array($social_media_links) && count($social_media_links) ) : ?>		     
-				   		<div class="social">
-				   		<?php foreach ($social_media_links as $key => $link): 
+				   		<div class="social"><?php 
+				   		foreach ($social_media_links as $key => $link): 
 							if (isset($link['image']['url'])): 
 								?><a href="<?php echo $link['link']; ?>" class="<?php echo $link['slug']; ?>" target="_blank">
-								        		<img src="<?php echo $link['image']['url']; ?>" alt="">
-								        		<span class="hide">Social Media Link <?php echo $link['name']; ?></span>
-								        	</a>
-							<?php else: ?><a href="<?php echo $link['link']; ?>" class="effect hvr-sweep-to-top icon-link <?php echo $link['slug']; ?>" target="_blank">
-								        		<i class="fa <?php echo $link['icon'].' '. $link['slug']; ?>" aria-hidden="true"></i>
-								        		<span class="hide">Social Media Link <?php echo $link['name']; ?></span>
-								        	</a><?php 
+					        		<img src="<?php echo $link['image']['url']; ?>" alt="">
+					        		<span class="hide">Social Media Link <?php echo $link['name']; ?></span>
+					        	</a><?php 
+					        	else: ?><a href="<?php echo $link['link']; ?>" class="effect hvr-sweep-to-top icon-link <?php echo $link['slug']; ?>" target="_blank">
+					        		<i class="fa <?php echo $link['icon'].' '. $link['slug']; ?>" aria-hidden="true"></i>
+					        		<span class="hide">Social Media Link <?php echo $link['name']; ?></span>
+					        	</a><?php 
 							endif;
 				        endforeach ?>
 				        </div>
